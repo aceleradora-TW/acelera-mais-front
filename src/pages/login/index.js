e6f2ae3f21b2ea79222ba0efe07e8e28e676e367
Erick Cardoso <cyber666king@gmail.com>
2021-10-14 14:38:03 -0300

    feat: cria pagina para login

---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import { Login } from './components/login'
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <h1>AceleraMais</h1>
+      <Login />
+    </div>
+  )
+}
+
+export default LoginPage
