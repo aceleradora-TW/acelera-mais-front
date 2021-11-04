592699c4ca58b8a9e5eb0090290436fed34ba810
inglyd <inglydalves@outlook.com>
2021-11-04 15:54:10 -0300

    fix: adiciona id nos processos
    @eluvya @inglyd
    
    16

--- src/pages/hiring-process/index.js
+++ src/pages/hiring-process/index.js
@@ -6,18 +6,21 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons'
 const HiringProcessPage = () => {
   const processes = [
     {
+      id: 1,
       name: '2021/1',
       startDate: new Date(2021, 11, 15).toISOString(),
       endDate: new Date(2021, 11, 21).toISOString(),
       description: 'A description'
     },
     {
+      id: 2,
       name: '2021/2',
       startDate: new Date(2021, 9, 22).toISOString(),
       endDate: new Date(2022, 10, 27).toISOString(),
       description: 'A description'
     },
     {
+      id: 3,
       name: '2021/2',
       startDate: new Date(2021, 8, 18).toISOString(),
       endDate: new Date(2021, 8, 19).toISOString(),
@@ -30,16 +33,16 @@ const HiringProcessPage = () => {
 
   return (
     <div className="hiring-page">
-        <h1>Processos seletivos</h1>
-        <section>
-            <div>
-            <Button classe={'button-filter'} text="Todos" onClick={handleSubmit} />
-            <Button classe={'button-filter'} text="Abertos" onClick={handleSubmit} />
-            <Button classe={'button-filter'} text="Fechados" onClick={handleSubmit} />
-            </div>
-            <Button icon={faPlus} classe={'button-new-process'} text="Novo processo" onClick={handleSubmit}/>
-        </section>
-        <ProcessList processes={processes} />
+      <h1>Processos seletivos</h1>
+      <section>
+        <div>
+          <Button classe={'button-filter'} text="Todos" onClick={handleSubmit} />
+          <Button classe={'button-filter'} text="Abertos" onClick={handleSubmit} />
+          <Button classe={'button-filter'} text="Fechados" onClick={handleSubmit} />
+        </div>
+        <Button icon={faPlus} classe={'button-new-process'} text="Novo processo" onClick={handleSubmit} />
+      </section>
+      <ProcessList processes={processes} />
     </div>
   )
 }
