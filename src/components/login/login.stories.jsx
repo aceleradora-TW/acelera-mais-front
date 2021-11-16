0699338177965fe1c03b2adcacf6d13ad079c356
Dhienifer <dhienifer083@gmail.com>
2021-11-16 14:45:50 -0300

    Atualizando componentes

--- src/components/login/login.stories.jsx
+++ src/components/login/login.stories.jsx
@@ -7,20 +7,10 @@ export default {
 
 const Template = args => <Login {...args} />
 
-<<<<<<< HEAD
-export const Logininput = Template.bind({})
-
-Date.args = {
-  label: 'Login',
-  onChange: () => { },
-  name: 'Login-input'
-
-=======
 export const loginInput = Template.bind({})
 
 loginInput.args = {
   label: 'Login',
   onChange: () => { },
   name: 'Login'
->>>>>>> 3c8129c40a4365d7f47091c14905f67ce88e2bf7
 }
