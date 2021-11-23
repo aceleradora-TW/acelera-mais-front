5665773844cca8bee0a94fc890068b3a17897e49
Gustavo1x <muchira003@gmail.com>
2021-11-23 15:24:46 -0300

    corrigido url do backEnd

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const { URL_BACKEND = 'http://localhost:9000/' } = process.env
+const { URL_BACKEND = 'https://test-acelera-mais-api.herokuapp.com/' } = process.env
 
 export const client = axios.create({
   baseURL: URL_BACKEND
