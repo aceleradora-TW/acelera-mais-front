e35fadb7a00d5525d6c725ee490c701085f35996
leonardo gomes <leonardogomespadilha@gmail.com>
2021-11-24 10:04:59 -0300

    feat(components): implementa lógica que exibe caixa de feedback quando o botão é clicado
    @leonardogomesp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import Routes from './routes'
+
 import { ToggleRow } from './components/togglerow'
 
 const App = () => {
   return (
-    <ToggleRow />
+    <ToggleRow item={'Usou x coisas de y formas'} />
     // <Routes />
   )
 }
--- src/components/togglerow/index.js
+++ src/components/togglerow/index.js
@@ -5,10 +5,25 @@ import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 
 export const ToggleRow = ({ item }) => {
   const [checked, setChecked] = useState(false)
-  // const style = checked ? 'toggle' : 'toggle'
+
+  const aberto = checked ? 'toggleRow' : 'toggleRow toggleRowChecked'
+
+  const fechado = checked ? 'toggleRows' : 'toggleRows toggleRowCheckeds'
+
   return (
-    <div className='toggleRow' onClick={() => setChecked(!checked)}>
-        <Icon icon={faAngleUp}/>{JSON.stringify(checked)}
-    </div>
+    <table>
+
+      <td className={aberto} onClick={() => setChecked(!checked)}>
+        <Icon icon={faAngleUp}/>
+      </td>
+
+      <td className={fechado}>
+        <tr>
+          <strong>Feedback:</strong>
+          <p>{ item }</p>
+        </tr>
+      </td>
+
+    </table>
   )
 }
