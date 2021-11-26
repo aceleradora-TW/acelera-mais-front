ba383ab86dbdb22dc10057e60a8919ce8e24b216
eluvya <eluvya@hotmail.com>
2021-11-26 15:46:32 -0300

    feat(evaluation-challenge): adicionado tags tr e table
    @eluvya @gustavo1x
    
    56

--- src/pages/evaluation-challenge/index.js
+++ src/pages/evaluation-challenge/index.js
@@ -4,16 +4,26 @@ import './style.css'
 
 export const EvaluationChallenge = () => {
   return (
-  <div className="table">
-  <h1>Avaliação</h1>
-    <div className="select">
-       <label htmlFor='select'>tipo</label>
-       <select name="select" id="select">
-       <option value="BackEnd" selected>BackEnd</option>
-       <option value="FrontEnd">FrontEnd</option>
-       </select>
-       </div>
-    </div>
+    <div className="table">
+      <h1>Avaliação</h1>
+      <div className="select">
+        <table>
+          <tr>
+            <td>
+              <label htmlFor='select'>Tipo:</label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <select name="select" id="select">
+                <option value="BackEnd" selected>BackEnd</option>
+                <option value="FrontEnd">FrontEnd</option>
+              </select>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div >
   )
 }
 export default EvaluationChallenge
