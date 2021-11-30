3f33086e79d3938f8e44022e41f8441dfff47fb0
patricia <patriciatersariol@gmail.com>
2021-11-30 10:38:32 -0300

    refactor(routes): retira rota de /form-create
    @patriciatersariol @jojorel
    
    65

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import LoginPage from '../pages/login-page/index.js'
 import HiringProcessPage from '../pages/hiring-process'
-import { HiringProcessForm } from '../components/forms/hiring-process'
 import { IsAuth } from '../pages/protected-route/index.js'
 
 const AppRoutes = () => {
@@ -12,9 +11,6 @@ const AppRoutes = () => {
         <Route path="/" element={<LoginPage />} exact />
         <Route path="/hiring-process" element={<IsAuth />} >
           <Route path="/hiring-process" element={<HiringProcessPage />} exact />
-        </Route>
-        <Route path="/form-create" element={<IsAuth />} >
-          <Route path="/form-create" element={<HiringProcessForm />} exact />
         </Route>
       </Routes>
     </BrowserRouter>
