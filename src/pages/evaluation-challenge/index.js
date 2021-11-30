a0c85d64dd6a195b8ba438e8af81bd136c698d56
Gustavo1x <muchira003@gmail.com>
2021-11-30 12:00:32 -0300

    adicionado icone

--- src/pages/evaluation-challenge/index.js
+++ src/pages/evaluation-challenge/index.js
@@ -1,25 +1,30 @@
 
 import { React } from 'react'
 import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPrint } from '@fortawesome/free-solid-svg-icons'
 
 export const EvaluationChallenge = () => {
   return (
-    <section className="table">
+    <><section className="table">
+      <div className="primeira">
+        <p>Avaliação </p>
+      </div>
 
-          <div className="primeira">
-            <p>Avaliação</p>
-            </div>
+      <div className="segunda">
+        <span>Tipo: </span>
+        <select name="select" id="select">
+          <option value="BackEnd" selected>BackEnd</option>
+          <option value="FrontEnd">FrontEnd</option>
+        </select>
+      </div>
+    </section>
+    <section className="head">
+        <div className="button">
+          <FontAwesomeIcon icon={faPrint} />
+        </div>
 
-             <div className="segunda">
-              <span>Tipo: </span>
-              <select name="select" id="select">
-                <option value="BackEnd" selected>BackEnd</option>
-                <option value="FrontEnd">FrontEnd</option>
-              </select>
-              </div>
-
-        </section>
-
+      </section></>
   )
 }
 export default EvaluationChallenge
