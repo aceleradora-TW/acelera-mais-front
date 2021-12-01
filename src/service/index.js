20583a648d5ceb2676384f5d78deb10b9406dd38
Ju Dantas <jucienyds@gmail.com>
2021-11-30 23:51:21 -0300

    feat: adiciona react-dotenv

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
+import env from 'react-dotenv'
 
-const { URL_BACKEND = 'https://test-acelera-mais-api.herokuapp.com/' } = process.env
+const { URL_BACKEND = 'http://localhost:9000' } = env
+
+console.log({ URL_BACKEND })
 
 export const client = axios.create({
   baseURL: URL_BACKEND
