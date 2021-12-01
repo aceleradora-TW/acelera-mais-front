af537603b4a3f35ab672ca6e02afd4b5122f527b
Bianca Salino <biancadsalino@gmail.com>
2021-12-01 09:54:42 -0300

    feat(components): implementando componente togglerow no exercise-list
    @biancasalino @leonardogomesp @biamah

--- src/components/togglerow/index.js
+++ src/components/togglerow/index.js
@@ -1,8 +1,9 @@
-import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import { faAngleUp, faPen } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 import Icon from '../icon'
 import { Status } from '../status'
 import './style.css'
+import ActionButton from '../buttons/action'
 
 const getStatus = ({ status }) => {
   if (status === 'status-preparing' || status === 'status-closed') return true
@@ -14,7 +15,7 @@ const hasFeedback = ({ status }) => {
   return false
 }
 
-export const ToggleRow = ({ item }) => {
+export const ToggleRow = ({ item, key }) => {
   const [checked, setChecked] = useState(false)
 
   const botao = checked ? 'table-row-btn' : 'table-row-btn table-row-btn-checked'
@@ -28,15 +29,21 @@ export const ToggleRow = ({ item }) => {
   }
   return (
     <>
-      <tr className='toggle-row-container'>
+      <tr key={key} className='toggle-row-container'>
         <td>{item.name}</td>
         <td>{item.type}</td>
-        <td colSpan='2' className='avaliador-colSpan'>
+        <td colSpan='2'>
           {item.status !== 'status-opened' ? < Status status={item.status} /> : null}
-          <button disabled={status}>Avaliar</button>
+          <ActionButton text={'avaliar'} icon={faPen} disabled={status} />
         </td>
-        <td>{
-          <button className={botao} disabled={hasFeedback(item)} onClick={handleClick}><Icon icon={faAngleUp} /></button>
+        <td className='avaliator-col'>{
+          <button
+            className={botao}
+            disabled={hasFeedback(item)}
+            onClick={handleClick}
+          >
+            <Icon icon={faAngleUp} />
+          </button>
         }</td>
       </tr>
       {status ? <tr><td colSpan='5' className={feedbackLine}>{feedback}</td></tr> : null}
