ceb39246ed28cd670fe1e0a79c19eeb7687056f3
eluvya <eluvyapiauilino@gmail.com>
2021-12-01 11:51:58 -0300

    feat(evaluation-challange): adiciona modal e botoes
    @gustavo1x @eluvya @jojorel
    
    56

--- src/pages/evaluation-challenge/index.js
+++ src/pages/evaluation-challenge/index.js
@@ -2,13 +2,16 @@
 import { React } from 'react'
 import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPrint } from '@fortawesome/free-solid-svg-icons'
+import { faPrint, faLink, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
+import Buttons from '../../components/buttons/default'
+import Button from '../../components/buttons/primary'
+import { Modal } from '../../components/modal'
 
 export const EvaluationChallenge = () => {
   return (
     <div className="page-container">
       <section className="head">
-          <h1>Avaliação </h1>
+        <h1>Avaliação </h1>
         <div className="select">
           <span>Tipo: </span>
           <select name="select" id="select">
@@ -17,12 +20,30 @@ export const EvaluationChallenge = () => {
           </select>
         </div>
       </section>
-      <section>
-        <div className="download">
-          <FontAwesomeIcon icon={faPrint} />
-          <a href="download/acme-doc-2.0.1.txt" download="Acme Documentation (ver. 2.0.1).txt">Download PDF - </a>
+      <div className="download">
+        <FontAwesomeIcon icon={faPrint} />
+        <a href="download/acme-doc-2.0.1.txt" download="Acme Documentation (ver. 2.0.1).txt">Download PDF - </a>
+      </div>
+      <div className="answer">
+        <h2>Respostas enviadas:</h2>
+        <div>
+          <FontAwesomeIcon icon={faLink} />
         </div>
-      </section>
+        <div>
+          <FontAwesomeIcon icon={faCodeBranch} />
+        </div>
+      </div>
+      <Buttons classe={'button-default'} text="cancelar"></Buttons>
+      <Modal classe={'button-primary'} text="avaliar" title="Avaliação">
+        <div className="select">
+          <span>Nota: </span>
+          <select name="select" id="select">
+            <option value="BackEnd" selected>BackEnd</option>
+            <option value="FrontEnd">FrontEnd</option>
+          </select>
+        </div>
+        <Button classe={'button-primary'} text="Enviar avaliação"></Button>
+      </Modal>
     </div>
   )
 }
