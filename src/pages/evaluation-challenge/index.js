ace346d8629f24f1b884af1193cdf1149d998208
eluvya <eluvyapiauilino@gmail.com>
2021-12-02 10:00:56 -0300

    style(evaluation-challenge): adiciona componentes na modal
    @gustavo1x @eluvya
    
    56

--- src/pages/evaluation-challenge/index.js
+++ src/pages/evaluation-challenge/index.js
@@ -6,6 +6,7 @@ import { faPrint, faLink, faCodeBranch } from '@fortawesome/free-solid-svg-icons
 import Buttons from '../../components/buttons/default'
 import Button from '../../components/buttons/primary'
 import { Modal } from '../../components/modal'
+import { Input } from '../../components/inputs'
 
 export const EvaluationChallenge = () => {
   return (
@@ -28,22 +29,23 @@ export const EvaluationChallenge = () => {
         <h2>Respostas enviadas:</h2>
         <div>
           <FontAwesomeIcon icon={faLink} />
-        </div>
-        <div>
           <FontAwesomeIcon icon={faCodeBranch} />
         </div>
       </div>
-      <Buttons classe={'button-default'} text="cancelar"></Buttons>
-      <Modal classe={'button-primary'} text="avaliar" title="Avaliação">
-        <div className="select">
-          <span>Nota: </span>
-          <select name="select" id="select">
-            <option value="BackEnd" selected>BackEnd</option>
-            <option value="FrontEnd">FrontEnd</option>
-          </select>
-        </div>
-        <Button classe={'button-primary'} text="Enviar avaliação"></Button>
-      </Modal>
+      <div className="buttons">
+        <Buttons classe={'button-default'} text="cancelar"></Buttons>
+        <Modal classe={'button-primary'} text="avaliar" title="Avaliação">
+          <div className="select">
+            <span>Nota: </span>
+            <select name="select" id="select">
+              <option value="BackEnd" selected>BackEnd</option>
+              <option value="FrontEnd">FrontEnd</option>
+            </select>
+          </div>
+          <Input type="text" name="feedback" label="Feedback:" />
+          <Button classe={'button-primary'} text="Enviar avaliação"></Button>
+        </Modal>
+      </div>
     </div>
   )
 }
