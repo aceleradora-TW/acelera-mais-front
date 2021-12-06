3e659cad077460513160b657c3476070d32cea0c
Gustavo1x <muchira003@gmail.com>
2021-12-06 11:38:38 -0300

    feat: trabalho em andamento

--- src/pages/evaluation-challenge/index.js
+++ src/pages/evaluation-challenge/index.js
@@ -3,8 +3,8 @@ import { React } from 'react'
 import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPrint, faLink, faCodeBranch } from '@fortawesome/free-solid-svg-icons'
-import Buttons from '../../components/buttons/default'
-import Button from '../../components/buttons/primary'
+import DefaultButton from '../../components/buttons/default'
+import PrimaryButton from '../../components/buttons/primary'
 import { Modal } from '../../components/modal'
 import exercises from '../../mocks/exercise-mock.json'
 import Select from '../../components/select'
@@ -13,16 +13,21 @@ export const EvaluationChallenge = () => {
   const exercise = exercises[0]
   return (
     <div className="page-container">
+
       <section className="head">
         <h1>Avaliação </h1>
+        <div className="">
           <Select label="Tipo:" placeholder="Escolha uma opção" options={[
             { label: 'Backend', value: 0 },
             { label: 'Frontend', value: 1 },
             { label: 'Fullstack', value: 2 }
           ]} ></Select>
+          <PrimaryButton text="alterar" />
+        </div>
       </section>
+
       <div className="download">
-      <FontAwesomeIcon icon={faPrint} />
+        <FontAwesomeIcon icon={faPrint} />
         <a href={exercise.links.pdf}> Download PDF</a>
       </div>
       <div className="answer">
@@ -33,19 +38,19 @@ export const EvaluationChallenge = () => {
         </div>
       </div>
       <div className="buttons">
-        <Buttons classe={'button-default'} text="cancelar"></Buttons>
+        <DefaultButton text="cancelar" />
         <Modal classe={'button-primary'} text="avaliar" title="Avaliação">
-        <div className="form-group">
-        <Select label="Nota:" placeholder="Escolha uma opção" options={[
-          { label: 0, value: 0 },
-          { label: 1, value: 1 },
-          { label: 2, value: 2 },
-          { label: 3, value: 3 },
-          { label: 4, value: 4 },
-          { label: 5, value: 5 }
-        ]} ></Select>
+          <div className="form-group">
+            <Select label="Nota:" placeholder="Escolha uma opção" options={[
+              { label: 0, value: 0 },
+              { label: 1, value: 1 },
+              { label: 2, value: 2 },
+              { label: 3, value: 3 },
+              { label: 4, value: 4 },
+              { label: 5, value: 5 }
+            ]} ></Select>
             <textarea label="mensagem" className="form-control" id="message-text"></textarea>
-          <Button classe={'button-primary'} text="Enviar avaliação"></Button>
+            <PrimaryButton text="Enviar avaliação" />
           </div>
         </Modal>
       </div>
