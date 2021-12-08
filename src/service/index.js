81122cf6a8d5923cc2443ca9168cab64495777bc
inglyd <inglydalves@outlook.com>
2021-12-08 11:56:41 -0300

    style: adiciona padding no botao de import
    @inglyd @LuizHenrique000
    
    72

--- src/service/index.js
+++ src/service/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import env from 'react-dotenv'
 
 const getURLBackend = () => {
-  return env ? env.URL_BACKEND : 'https://luiz-acelera-mais-api-teste.herokuapp.com'
+  return env ? env.URL_BACKEND : 'http://localhost:9000'
 }
 
 export const client = axios.create({
