fc027893cc672172338cb1a286d899a3bf4ba8ec
Dhienifer <dhienifer083@gmail.com>
2022-01-07 13:25:20 -0300

    refatora componente select @dhierodrigues @estherwl @eluvya

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Select = styled.select`
+export const Container = styled.div`
   display: flex;
   justify-content: flex-start;
   margin-bottom: 5px;
