9ec586b06f75cf2a1545c0e1b988a76521ed3067
Carolina Borges <carolinaborgescardoso@gmail.com>
2022-01-12 10:15:31 -0300

    refactor: continua estilização em componentes @ccarolb

--- src/pages/evaluation/styled.js
+++ src/pages/evaluation/styled.js
@@ -1,8 +1,46 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-text-align: justify;
+  text-align: justify;
   padding: 30px;
   width: 100%;
+  margin-top: 20px;
+`
+export const StyledHeader = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+h1 {
+  font-size: 40px;
+  color: rgb(149, 149, 149);
+  font-weight: bold;
+  font-family: Arial, Helvetica, sans-serif;
+} 
+`
+
+export const Download = styled.div`
+  border: 1px solid #ababab;
+  border-radius: 5px;
+  width: 100%;
+  margin-top: 20px;
+  padding: 10px;
+  font-family: Arial, Helvetica, sans-serif;
+`
+export const StyledAnswer = styled.div`
+  border: 1px solid #ababab;
+  border-radius: 5px;
+  width: 100%;
+  margin-top: 20px;
+  padding: 10px;
+  font-family: Arial, Helvetica, sans-serif;
+
+h2 {
+  color: rgb(149, 149, 149);
+  font-family: Arial, Helvetica, sans-serif;
+}
+`
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: right;
   margin-top: 20px;
 `
