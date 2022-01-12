ec30fe7d1fed294036ccfb2b66aa06504988b902
Cibely Pereira <cibely.port@gmail.com>
2022-01-12 14:41:46 -0300

    refactor(pages\exercises\toggle-row): mudança de nomenclatura
    @estherwl @Cibely87
    
    47

--- src/pages/exercises/components/toggle-row/index.js
+++ src/pages/exercises/components/toggle-row/index.js
@@ -5,26 +5,7 @@ import Button from '../../../../components/buttons/button'
 import { Status } from '../../../../components/status'
 import { client } from '../../../../service'
 import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
-
-const Tr = styled.tr`
-  button.toggle-off:disabled{
-    opacity: 0.4 ;
-    cursor: no-drop;
-  }
-
-  button.toggle-on{
-    -moz-transform: scale(1, -1);
-    -webkit-transform: scale(1, -1);
-    -o-transform: scale(1, -1);
-    -ms-transform: scale(1, -1);
-    transform: scale(1, -1);
-  }
-
-  .options button{
-    margin-left: 20px;
-  }
-`
+import { Tr } from './styled'
 
 const statusEnum = {
   PREPARING: 'status-preparing',
