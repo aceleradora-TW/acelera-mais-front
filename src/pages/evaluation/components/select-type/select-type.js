fde95bf5696df28ea6f3890c9f1e0185be1983bf
Carolina Borges <carolinaborgescardoso@gmail.com>
2022-01-12 11:17:29 -0300

    refactor: componente styled type ok @ccarolb

--- src/pages/evaluation/components/select-type/select-type.js
+++ src/pages/evaluation/components/select-type/select-type.js
@@ -2,7 +2,7 @@ import { React, useState } from 'react'
 import Select from '../../../../components/select'
 import { client } from '../../../../service'
 import SucessButton from '../../../../components/buttons/sucess'
-// import { SelectContainer } from '../../styled'
+import { SelectContainer } from '../../styled'
 
 export const Type = ({ setDisableEvaluationButton }) => {
   const [exerciseTypeSelected, setExerciseTypeSelected] = useState(false)
@@ -16,8 +16,8 @@ export const Type = ({ setDisableEvaluationButton }) => {
 
   return (
 
-    // <SelectContainer>
-    <div className='select-container'>
+    <SelectContainer>
+      {/* // <div className='select-container'> */}
       <Select
         onChange={({ target }) => {
           setExerciseType(target.value)
@@ -31,15 +31,17 @@ export const Type = ({ setDisableEvaluationButton }) => {
           { label: 'Fullstack', value: 'Fullstack' }
         ]} />
 
-      {exerciseTypeSelected
-        ? <SucessButton text="Alterar" onClick={() => {
-          setDisableEvaluationButton(false)
-          setExerciseTypeSelected(false)
-          handleTypeSubmit()
-          alert('Alterado com sucesso!')
-        }} />
-        : null}
-      {/* </SelectContainer> */}
-    </div>
+      {
+        exerciseTypeSelected
+          ? <SucessButton text="Alterar" onClick={() => {
+            setDisableEvaluationButton(false)
+            setExerciseTypeSelected(false)
+            handleTypeSubmit()
+            alert('Alterado com sucesso!')
+          }} />
+          : null
+      }
+    </SelectContainer>
+    // </div >
   )
 }
