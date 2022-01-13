34d92c89866288b774cd72f804539f56a5b8b27e
Ju Dantas <jucienyds@gmail.com>
2022-01-13 02:06:21 -0300

    feat: cria o componente anchor

---
+++ src/pages/evaluation/styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Subtitle = styled.h2`
+  margin-bottom: 10px;
+  color: rgb(149, 149, 149);
+`
+
+export const Anchor = styled.a`
+  background-color: #ddd;
+  color: black;
+  text-decoration: none;
+  padding: 10px;
+  display: inline-block;
+  border-radius: 5px;
+  text-transform: uppercase;
+  font-size: 13px;
+  font-weight: bold;
+
+  svg {
+    margin-right: 5px;
+  }
+`
