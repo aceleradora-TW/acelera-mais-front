9d4ca92eb09f0c8397f9fce6724f1dab9f1eda30
estherwl <estherlucena@gmail.com>
2022-01-21 11:16:16 -0300

    "refact(components/select): modifica espaçamento"

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 export const StyledSelectContainer = styled.div`
   display: flex;
   flex-direction: column;
+  margin-bottom: 5px;
   justify-content: flex-start;
   font-weight: bold;
   
   & > label {
-    margin-bottom: 5px;
+    margin-bottom: 8px;
   }
 
 `
