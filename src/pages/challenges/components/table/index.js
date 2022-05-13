d3d2fb61cd3154607e8cb1077dc347088a8c9cac
Eluvya Teles Piauilino <eluvyapiauilino@gmail.com>
2022-05-13 11:55:56 -0300

    feat(challenges): reutiliza components
    @eluvya @Cibely87
    
    137

--- src/pages/challenges/components/table/index.js
+++ src/pages/challenges/components/table/index.js
@@ -2,21 +2,15 @@ import { useEffect, useState } from 'react'
 import { client } from '../../../../service'
 import { ChallengeTable, Container } from './styled'
 import { useTranslation } from 'react-i18next'
-// import challengeBD from './challenge.json'
-import { challengesAdapter } from '../../adapters/challenges-adapter'
-import { ToggleRowExercise } from '../toggle-row-exercise'
-import { TableBody } from '../table-body'
 
 export const Table = ({ adapter, BodyComponent, RowComponent }) => {
   const { t } = useTranslation()
   const [challenges, setChallenges] = useState([])
 
   useEffect(() => {
-    /* const challengesAdapted = challengesAdapter(challengeBD.data.result)
-    setChallenges(challengesAdapted) */
     const id = window.location.pathname.split('/').pop()
     client.get(`/challenge?hiringProcessId=${id}`)
-      .then(res => setChallenges(challengesAdapter(res.data.data.result)))
+      .then(res => setChallenges(adapter(res.data.data.result)))
       .catch(err => {
         alert(t('challenge.alert'))
         console.log(err)
@@ -35,9 +29,9 @@ export const Table = ({ adapter, BodyComponent, RowComponent }) => {
             <th>Feedbacks</th>
           </tr>
         </thead>
-        <TableBody
+        <BodyComponent
           items={challenges}
-          RowComponent={ToggleRowExercise}
+          RowComponent={RowComponent}
         />
       </ChallengeTable>
     </Container>
