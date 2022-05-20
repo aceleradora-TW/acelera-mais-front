2dc852a8fbe183472e8a2c74b4808677d167e1e5
Emilly <emillymarques96622@gmail.com>
2022-05-20 14:30:38 -0300

    implementação do button e h1

--- src/pages/mentor-registration/index.js
+++ src/pages/mentor-registration/index.js
@@ -1,19 +1,17 @@
 import { useTranslation } from 'react-i18next'
 import Button from '../../components/buttons/button'
-import showFeature from '../../utils/feature-toggle'
+
 const MentorRegistrationPage = () => {
-  const { t } = useTranslation
+  const { t } = useTranslation()
   const handleSubmit = 0
   return (
     <div className="page-container">
       <section>
-        <h1>{t('mentorregistration.title')}</h1>
-        {showFeature()
-          ? (<div>
-            <Button classe="button-filter" text={t('hiringProcess.filter.all')} onClick={handleSubmit} />
-            </div>)
-            : null}
-      </section>
+        <h1>{t('lista de mentoas avaliadoras')}</h1>
+            <div>
+              <Button text={t('cadastrar nova mentora')} onClick={handleSubmit} />
+            </div>
+       </section>
     </div>
   )
 }
