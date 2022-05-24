07fb45d519ef35d61e27bb1ddf73fff0fc6025db
Dhienifer <dhienifer083@gmail.com>
2022-05-24 10:26:25 -0300

    fix(pages/home):corrige os nomes dos botoes para ingles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,25 @@
+import PrimaryButton from '../../components/buttons/primary'
 const HomePage = () => {
+
+  const handleSubmit = () => {
+    location.reload()
+  }
   return (
-    <Container>
-      <h1>Página inicial</h1>
-    </Container>
+    <div className="pages-home">
+
+      <h1>Home page</h1>
+
+      <div className="button-homeclear">
+        <PrimaryButton text={t('Selective process')} onClick={handleSubmit} />
+        <PrimaryButton text={t('Registration of mentors')} onClick={handleSubmit} />
+
+      </div>
+    </div>
   )
+
+
+
+
 }
 
 export default HomePage
