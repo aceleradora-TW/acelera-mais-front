963e22de8cdff4abd5a350593ecc9d4103e673d6
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-05-25 10:46:39 -0300

    componente email

--- src/components/inputs/email/index.js
+++ src/components/inputs/email/index.js
@@ -1,11 +1,23 @@
 import { InputBox } from '../styled'
 import { Input } from '../index'
+import { useState } from 'react'
+import { validator } from 'validator'
 
-export const InputEmail = ({ onChange, label, name }) => {
+export const InputEmail = ({ label, name }) => {
+  const [emailError, setEmailError] = useState('')
+  const change = (e) => {
+    const email = e.target.value
+    if (validator.isEmail(email)) {
+      setEmailError('Email valido')
+    } else {
+      setEmailError('O email não é valido!')
+    }
+  }
   return (
     <InputBox>
       <span>{label}</span>
-      <Input type="email" name={name} onChange={onChange} />
+      <Input type="text" name={name} onChange={change} />
+      <span>{emailError}</span>
     </InputBox>
   )
 }
