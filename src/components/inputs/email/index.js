1828464d5f7923d4000667cef46f79f7fc75e395
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-05-27 10:27:02 -0300

    Conclusao do componente email

--- src/components/inputs/email/index.js
+++ src/components/inputs/email/index.js
@@ -2,20 +2,24 @@ import { InputBox } from '../styled'
 import { Input } from '../index'
 import { useState } from 'react'
 import validator from 'validator'
+import styled from 'styled-components'
 
+const MessageError = styled.span`
+  font-size: 16px;
+  color: #ff3d00
+`
 export const InputEmail = ({ label, name }) => {
   const [emailError, setEmailError] = useState('')
   const change = (e) => {
     const email = e.target.value
-    if (!validator.isEmail(email)) {
-      setEmailError('Email invalido!')
-    }
+    const message = validator.isEmail(email) ? '' : 'Email invalido!'
+    setEmailError(message)
   }
   return (
     <InputBox>
       <span>{label}</span>
       <Input type="text" name={name} onChange={change} />
-      <span>{emailError}</span>
+      <MessageError>{emailError}</MessageError>
     </InputBox>
   )
 }
