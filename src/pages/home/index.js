cf195cdc2a4ec3dd5a90a987b386c847d5c883d7
cintia <cintia.silvadeooliveira@gmail.com>
2022-06-06 14:03:49 -0300

    imprementar usetranslation

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,19 @@
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
 import { HomePageContainer, HomeContainer } from './styled'
 
 export const HomePage = () => {
+  const { t } = useTranslation()
   return (
     <HomePageContainer>
-      <h1>Página inicial</h1>
+      <h1>{t('user.title')}</h1>
       <HomeContainer>
-        <Link to="/hiring-process">Processo seletivo
+        <Link to="/hiring-process">{t('user.hiring-process')}
           < FontAwesomeIcon icon={faChevronCircleRight}/>
         </Link>
-        <Link to="/manage-Mentors">Gerenciamento de mentoras
+        <Link to="/user">Gerenciamento de mentoras{t('user.manage-mentor')}
         <FontAwesomeIcon icon={faChevronCircleRight}/>
         </Link>
       </HomeContainer>
