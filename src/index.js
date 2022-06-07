9ae86244dec45ec36bbd1ab7a2a30d8ba9b738b1
Emilly <emillymarques96622@gmail.com>
2022-06-07 12:02:35 -0300

    implementação de form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import EditInfoMentor from './pages/mentor-list/edit-info'
+// import App from './App'
 // import reportWebVitals from './reportWebVitals'
 import './utils/i18next'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <EditInfoMentor />
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/pages/mentor-list/edit-info.js
+++ src/pages/mentor-list/edit-info.js
@@ -1,3 +1,16 @@
-export const EditInfoMentor = () => { 
+import { useTranslation } from 'react-i18next'
+export const EditInfoMentor = () => {  
+  return (
+<form id="formulario">
+<label htmlFor="nome">{useTranslation('user.descriptionTable.name')}</label>
+<input type="text" name="nome" id="nome"/>
 
+<label htmlFor="telephone">{useTranslation('user.descriptionTable.name')}</label>
+<input type="submit" name="enviar" value="enviar"/>
+
+<label htmlFor="email">{useTranslation('user.descriptionTable.name')}</label>
+<input type="submit" name="enviar"/>
+</form> 
+  ) 
 }
+export default EditInfoMentor
