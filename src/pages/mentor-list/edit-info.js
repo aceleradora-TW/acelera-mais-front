cc7ad50d3272aea22905f16d22d36ad64df95b79
ludmila <ludmilakdk@gmail.com>
2022-06-08 13:19:45 -0300

    criacao de componentes de estilizaçao

--- src/pages/mentor-list/edit-info.js
+++ src/pages/mentor-list/edit-info.js
@@ -1,18 +1,20 @@
 import { InputText } from '../../components/inputs/text'
 import { useTranslation } from 'react-i18next'
-import { Container } from './styled.js'
+import { PopUp, Title } from './styled.js'
 
 export const EditInfoMentor = () => {
   const { t } = useTranslation()
   return (
-    <Container>
+    <Title>
       <h1>{t('popupTitle.name')}</h1>
-      <form>
-        <InputText name="name" label={t('descriptionForm.name')}/>
-        <InputText name="telephone" label={t('descriptionForm.telephone')}/>
-        <InputText name="email" label={t('descriptionForm.email')}/>
-      </form>
-    </Container>
+      <PopUp>
+        <form>
+          <InputText name="name" label={t('descriptionForm.name')}/>
+          <InputText name="telephone" label={t('descriptionForm.telephone')}/>
+          <InputText name="email" label={t('descriptionForm.email')}/>
+        </form>
+      </PopUp>
+    </Title>
   )
 }
 export default EditInfoMentor
--- src/pages/mentor-list/styled.js
+++ src/pages/mentor-list/styled.js
@@ -1,29 +1,45 @@
-
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
   margin-top: 20px;
   padding: 40px;
-  Table{
+  Table {
     font-weight: bold;
     color: #000;
-
   }
-`
+`;
 
 export const Page = styled.div`
   text-align: justify;
-  padding:20px;
+  padding: 20px;
 
-h1 {
+  h1 {
     font-size: 40px;
     color: rgb(149, 149, 149);
     font-weight: bold;
   }
-  
+
   section {
     display: flex;
     justify-content: space-between;
   }
-  
-`
+`;
+export const PopUp = styled.div`
+  justify-content: center;
+  border: solid 1px;
+  max-width: 500px 500px;
+`;
+
+export const Title = styled.div`
+  margin-top: 5px;
+  padding: 40px;
+  border: solid 1px;
+  border-radius: 25px;
+  h1 {
+    margin-top: 20px;
+    background-color: rgb(149, 149, 149);
+    font-size: 20px;
+    color: #000000;
+    font-weight: bold;
+  }
+`;
