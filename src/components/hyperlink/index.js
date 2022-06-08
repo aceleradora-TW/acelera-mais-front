070c0e27b2684130255ef70f861789406508c889
cintia <cintia.silvadeooliveira@gmail.com>
2022-06-08 15:26:24 -0300

    refactor(src/components/hyperlink): troca de nome do componente
    @cintia89 @janinelps
    
    108

--- src/components/hyperlink/index.js
+++ src/components/hyperlink/index.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 
-export const Links = ({ link, label, icon }) => {
+export const Hyperlink = ({ link, label, icon }) => {
   return (
     <Link to={link} >{label}
       {icon ? < FontAwesomeIcon icon={icon} /> : null}
