8e2a3687219c06862f4c502217b04a56ed36a9ec
ludmila <ludmilakdk@gmail.com>
2022-06-10 10:39:03 -0300

    implementacao da estrutura de formulario

--- src/pages/user/forms/user/index.js
+++ src/pages/user/forms/user/index.js
@@ -1,6 +1,20 @@
-
+import { useTranslation } from 'react-i18next'
 export const UserForm = () => {
+  const { t } = useTranslation()
   return (
+    <>
     <h1> olaaaa</h1>
+    <form>
+      <InputText name="name" label={t('hiringProcess.form.name')} onChange={handleChange} />
+      <InputDate name="startDate" label={t('hiringProcess.form.startDate')} onChange={handleChange} />
+      <InputDate name="endDate" label={t('hiringProcess.form.endDate')} onChange={handleChange} />
+      <InputText name="description" label={t('hiringProcess.form.description')} onChange={handleChange} />
+      <PrimaryButton text={t('hiringProcess.form.submitButton')} onClick={sendHiringProcess} >
+        Enviar
+        </ PrimaryButton>
+      formi
+    
+    </form>
+    </>
   )
 }
