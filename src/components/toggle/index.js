3c43dddf2b843f8068d1fcf53acfdb37273dbcf8
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-06-13 15:58:15 -0300

    feat(components/toggle): adiciona styled no toggle button
    @drebarcelos @leticianunes8

--- src/components/toggle/index.js
+++ src/components/toggle/index.js
@@ -1,7 +1,67 @@
-export const ToggleButton = ({ label = { on: 'On', off: 'Off' } }) => {
+import styled from 'styled-components'
+import { useState } from 'react'
+
+const Button = styled.button`
+  border: 1px solid #d3d3d3;
+  border-radius: 5px;
+  padding: 10px 25px;
+  font-weight: 300px;
+  font-size: 16px;
+  background-color: white;
+  color: rgb(102,102,255);
+  border-color: rgb(102,102,255);
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.7;
+  }
+`
+
+// const ButtonEnabled = styled.button`
+//   border: 1px solid #d3d3d3;
+//   border-radius: 5px;
+//   padding: 10px 25px;
+//   font-weight: 300px;
+//   font-size: 16px;
+//   color: rgb(102,102,255);
+//   border-color: rgb(102,102,255);
+//   background-color: white;
+
+//   &:hover {
+//     cursor: pointer;
+//     opacity: 0.7;
+//   }
+// `
+// const ButtonDisabled = styled.button`
+//   border: 1px solid #d3d3d3;
+//   border-radius: 5px;
+//   padding: 10px 25px;
+//   font-weight: 300px;
+//   font-size: 16px;
+//   color: red;
+//   border-color: red;
+//   background-color: white;
+
+//   &:hover {
+//     cursor: pointer;
+//     opacity: 0.7;
+//   }
+// `
+
+export const ToggleButton = ({
+  label = {
+    on: 'On',
+    off: 'Off'
+  }
+}) => {
+  const [ischecked, setChecked] = useState(label.on)
+  const change = () => {
+    setChecked((prevState) => !prevState)
+  }
+
   return (
-    <button>{label.on}
-    Habilitar
-    </button>
+    <Button onClick={change}>
+      <span>{ischecked ? 'Habilitar' : 'Desabilitar'}</span>
+    </Button>
   )
 }
