fbe7165c83c26ade239b583bfb465b0c19847b96
ana.jess <ajmoreira98@gmail.com>
2022-06-21 14:53:59 -0300

    Alterações dos paramentros

--- src/components/status/index.js
+++ src/components/status/index.js
@@ -4,21 +4,28 @@ import { useTranslation } from 'react-i18next'
 
 export const Status = ({
   status, options = {
-    opened: 'status.opened',
-    closed: 'status.closed',
-    preparing: 'status.preparing'
+    status: {
+      green: 'status-opened',
+      red: 'status-closed',
+      yellow: {'status-preparing', 'first-login', 'email-resent'}
+    }
+    label: {
+      green: 'status.opened',
+      red: 'status.closed',
+      yellow: 'status.preparing'
+    }
   }
 }) => {
   const { t } = useTranslation()
 
   let label = t(options.closed)
 
-  if (status === 'status-opened') {
-    label = t(options.opened)
+  if (options.status.green.includes(status)) {
+    label = t(options.label.green)
   }
 
-  if (status === 'status-preparing') {
-    label = t(options.preparing)
+  if (options.status.yellow.includes(status)) {
+    label = t(options.label.yellow)
   }
   return (
     <StyledSpan className={status}>{label}</StyledSpan>
