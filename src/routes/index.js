d473f33f358a2b36e487f0073d14a862c36545cd
Emilly <emillymarques96622@gmail.com>
2022-06-27 15:31:00 -0300

    refator: mudança do nome de rota e do componente

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import LoginPage from '../pages/authentication/index'
 import HiringProcessPage from '../pages/hiring-process'
-import { Reset } from '../pages/password-reset'
+import { Reset } from '../pages/UserChangePasswordPage'
 import { HomePage } from '../pages/home'
 import { IsAuth } from '../components/is-auth/index.js'
 import { ChallengePage } from '../pages/challenges'
@@ -12,13 +12,13 @@ const AppRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} exact />
-        <Route path="/password-reset/:id" element={<Reset />} exact />
         <Route path="/home" element={<HomePage />} exact />
         <Route path="/hiring-process" element={<IsAuth />}>
           <Route path="/hiring-process" element={<HiringProcessPage />} exact />
         </Route>
         <Route path="/user" element={<IsAuth />}>
           <Route path="/user" element={<MentorPage />} exact />
+          <Route path="user/password-reset/:id" element={<Reset />} exact />
         </Route>
         <Route path="/challenges" element={<IsAuth />}>
           <Route path="/challenges/hiring-process/:id" element={<ChallengePage />} exact />
