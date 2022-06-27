e0aad6fb48b790e33afd2acb51ebe68479fe891c
Cibely Pereira <cibely.port@gmail.com>
2022-06-27 13:13:58 -0300

    feat(pages/password-reset): mudança da flag e redirecionamento para página home após a mudança de se
    BREAKING CHANGE: @MarquesEmilly @Cibely87

--- src/pages/password-reset/index.js
+++ src/pages/password-reset/index.js
@@ -11,12 +11,13 @@ import { InputPassword } from '../../components/inputs/password'
 import { useState } from 'react'
 import { client } from '../../service'
 import { t } from 'i18next'
+import { useNavigate } from 'react-router'
 
 export const Reset = () => {
   const [password, setPassword] = useState('')
   const [newPassword, setNewPassword] = useState('')
   const [message, setMessage] = useState('')
-  const [flag, setFlag] = useState('')
+  const navigate = useNavigate()
 
   const handleChangePassword = ({ target }) => {
     setPassword(target.value)
@@ -29,21 +30,21 @@ export const Reset = () => {
     event.preventDefault()
     const id = window.location.pathname.split('/').pop()
     if (password === newPassword && password && newPassword) {
-      setFlag('user-enabled')
       const user = {
-        password, flag
+        password, flag: 'user-enabled'
       }
 
       await client.put(`/user/${id}`, user)
         .then(res => {
-          // window.location.replace = '/'
+          alert(res.data.message)
+          navigate('/')
           return res
         }).catch(err => console.log(err))
 
       return true
     }
 
-    setMessage('As senhas estão diferentes ou em branco')
+    setMessage(t('reset.messageError'))
   }
 
   return (
@@ -56,7 +57,7 @@ export const Reset = () => {
           <InputPassword onChange={handleChangePassword} value={password} label={t('reset.password')} />
           <InputPassword onChange={handleChangeNewPassword} value={newPassword} label={t('reset.newPassword')} />
           <MessageError>{message}</MessageError>
-          <PrimaryButton text={t('reset.button')} onClick={handlerClick}></PrimaryButton>
+          <PrimaryButton text={t('reset.buttonSave')} onClick={handlerClick}></PrimaryButton>
         </ModalBody>
       </Content>
     </ContainerReset>
