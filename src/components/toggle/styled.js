2f9c127b2c53326b63b012b50616151f6e514894
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-06-27 14:32:04 -0300

    style(toggle): retira comentarios do arquivo styled
    @drebarcelos @leticianunes8

--- src/components/toggle/styled.js
+++ src/components/toggle/styled.js
@@ -32,34 +32,3 @@ export const ButtonDisabled = styled.button`
     opacity: 0.7;
   }
 `
-
-// const ButtonEnabled = styled.button`
-//   border: 1px solid #d3d3d3;
-//   border-radius: 5px;
-//   padding: 10px 25px;
-//   font-weight: 300px;
-//   font-size: 16px;
-//   color: rgb(102,102,255);
-//   border-color: rgb(102,102,255);
-//   background-color: white;
-
-//   &:hover {
-//     cursor: pointer;
-//     opacity: 0.7;
-//   }
-// `
-// const ButtonDisabled = styled.button`
-//   border: 1px solid #d3d3d3;
-//   border-radius: 5px;
-//   padding: 10px 25px;
-//   font-weight: 300px;
-//   font-size: 16px;
-//   color: red;
-//   border-color: red;
-//   background-color: white;
-
-//   &:hover {
-//     cursor: pointer;
-//     opacity: 0.7;
-//   }
-// `
