df809ed6ae4c2943b4fe8f7d429d6a4ed876b1ce
Leticia <leticianunesq8@gmail.com>
2022-07-11 15:06:57 -0300

    fix(src/pages/evaluation): traz um exercicio por id
    @leticianunes8 @jcdantss
    
    134

--- src/pages/evaluation/index.js
+++ src/pages/evaluation/index.js
@@ -11,23 +11,23 @@ import { Container, Download, ContainerButtons, Anchor } from './styled'
 import { useTranslation } from 'react-i18next'
 
 const EvaluationChallenge = () => {
-  const [challenge, setChallenge] = useState(null)
+  const [exercise, setExercise] = useState(null)
   const [disableEvaluationButton, setDisableEvaluationButton] = useState(true)
   const { t } = useTranslation()
 
-  const challengeId = window.location.pathname.split('/')[2]
+  const exerciseId = window.location.pathname.split('/')[2]
 
   useEffect(() => {
-    client.get(`/challenge/${challengeId}`)
+    client.get(`/exercise/${exerciseId}`) // traz informacao do back atraves do endpoint
       .then(res => (res.data))
-      .then(res => setChallenge(res))
+      .then(res => setExercise(res))
       .catch(err => {
         console.log(err)
       })
   }, [])
 
   const handleCancel = () => {
-    challenge.exercises.map((exercise) => {
+    exercise.exercises.map((exercise) => {
       return (
         client.patch(`evaluation/${exercise.id}`, { mentorName: 'cancelado' }),
         history.back()
@@ -35,19 +35,19 @@ const EvaluationChallenge = () => {
     })
   }
 
-  if (!challenge) return null
+  if (!exercise) return null
 
+  const exerciseName = exercise.name
+  console.log(exerciseName)
   return (
     <>
       <Container >
         <h1>{t('evaluation.title')}</h1>
-        {challenge.exercises.map((exercise) => {
-          return (
             <div key={exercise.id}>
               <Header setDisableEvaluationButton={setDisableEvaluationButton} />
 
               <Download>
-                <Anchor href="#" target='_blank' rel='noreferrer'>
+                <Anchor href={exerciseName} target='_blank' rel='noreferrer'>
                   <FontAwesomeIcon icon={faPrint} />
                   Download: {exercise.name}
                 </Anchor>
@@ -66,9 +66,6 @@ const EvaluationChallenge = () => {
 
               </ContainerButtons>
             </div>
-          )
-        })
-        }
       </Container >
     </>
   )
