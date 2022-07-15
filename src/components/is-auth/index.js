0460d8dd3d56833fed42a7ebe5a32a130459b23b
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-07-15 14:09:55 -0300

    feat(user): adiciona autentificacao de mentora admin na pagina de listagem
    @drebarcelos @lodmina

--- src/components/is-auth/index.js
+++ src/components/is-auth/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
 
-export const IsAuth = (props) => {
+export const IsAuth = ({ mentorType = ['admin', 'mentor'] }) => {
   const location = useLocation()
+  const role = localStorage.getItem('role')
 
-  if (localStorage.getItem('token')) {
+  if (localStorage.getItem('token') && mentorType.includes(role)) {
     return <Outlet />
   }
   return <Navigate to="/" state={{ from: location }} />
