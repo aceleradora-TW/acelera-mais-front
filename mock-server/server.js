205b789b1e5698e4dba1438afd3987d27a857863
Ju Dantas <jucienyds@gmail.com>
2022-09-16 23:58:52 -0300

    feat(mock-server): adiciona o servidor fake para desenvolvimento
    esse servidor fake ira ajudar durante o desenvolvimento das interfaces, para que as alunas nao
    precisem ter o backend rodando em suas maquinas

---
+++ mock-server/server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 9000
+
+const {
+  loginResponse,
+  userResponse,
+  emailVerificationResponse,
+  hiringProcessResponse,
+  challengeResponse,
+  exerciseResponse
+} = require('./mocks')
+
+app.use(cors())
+
+app.get('/', (req, res) => res.send("it's works!"))
+app.post('/login', (req, res) => res.json(loginResponse))
+app.get('/hiring_process', (req, res) => res.json(hiringProcessResponse))
+app.post('/hiring_process', (req, res) => res.status(200))
+app.patch('/hiring_process/:id', (req, res) => res.status(200))
+app.delete('/hiring_process/:id', (req, res) => res.status(200))
+
+app.get('/candidate', (req, res) => res.status(200))
+app.get('/candidate/:id', (req, res) => res.status(200))
+app.get('/candidate/challenge/hiring_process/:id', (req, res) => res.status(200))
+app.post('/candidate/hiring_process/:id', (req, res) => res.status(200))
+
+app.get('/evaluation', (req, res) => res.status(200))
+app.get('/evaluation/:id', (req, res) => res.status(200))
+app.patch('/evaluation/:id', (req, res) => res.status(200))
+app.post('/evaluation', (req, res) => res.status(200))
+app.delete('/evaluation/:id', (req, res) => res.status(200))
+
+app.get('/challenge', (req, res) => res.json(challengeResponse))
+app.get('/challenge/:id', (req, res) => res.status(200))
+app.patch('/challenge/:id', (req, res) => res.status(200))
+app.post('/challenge/hiring_process/:id', (req, res) => res.status(200))
+
+app.post('/user', (req, res) => res.status(200))
+app.get('/user', (req, res) => res.json(userResponse))
+app.put('/user/:id', (req, res) => res.status(200))
+app.put('/user/:id/email_verification', (req, res) => res.json(emailVerificationResponse))
+app.delete('/user/:id', (req, res) => res.status(200))
+
+app.get('/exercise/:id', (req, res) => res.json(exerciseResponse))
+app.put('/exercise/:id', (req, res) => res.status(200))
+
+app.listen(port, () => {
+  console.info(`Mocking Server running in localhost:${port}`)
+})
--- src/service/index.js
+++ src/service/index.js
@@ -1,24 +1,17 @@
 import axios from 'axios'
-import env from 'react-dotenv'
 
-const getURLBackend = () => {
-  if (env) {
-    const { URL_BACKEND } = env
-    localStorage.setItem('service', URL_BACKEND)
-    return URL_BACKEND
-  }
-  const service = localStorage.getItem('service')
-  return service && service !== '' ? service : 'http://localhost:9000'
-}
-
-export const client = axios.create({
-  baseURL: getURLBackend(),
-  headers: {
-    common: {
-      Authorization: `Bearer ${localStorage.getItem('token') || ''}`
+const getClientConfiguration = () => {
+  return {
+    baseURL: process.env.REACT_APP_BASEURL,
+    headers: {
+      common: {
+        Authorization: `Bearer ${localStorage.getItem('token') || ''}`
+      }
     }
   }
-})
+}
+
+export const client = axios.create(getClientConfiguration())
 
 export const setTokenInHeaders = (token = '') => {
   client.defaults.headers.common = {
