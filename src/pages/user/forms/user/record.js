ad5a6ce8266ee02c0788347feb6ecb81e1ef0918
Enzo Soares <soaressenzo@gmail.com>
2022-10-24 11:32:36 -0300

    FEAT: Created the verify URL and used the conditional rendering

--- src/pages/user/forms/user/record.js
+++ src/pages/user/forms/user/record.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router'
 import { client } from '../../../../service'
-// import { client } from '../../../../service'
+import { UserForm } from '.'
 
 export const RecordPage = () => {
   const { token } = useParams()
-  client.get(`/user/link_validation/${token}`)
-    .then(res => {
+  const [verify, setVerify] = useState([])
 
-    })
+  useEffect(() => {
+    client.get(`/user/link_validation/${token}`)
+      .then(res => {
+        setVerify(res.data.data.verified)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
+  console.log(verify)
+  if (verify === true) {
+    return (
+      <div>
+        <UserForm />
+      </div>
+    )
+  }
   return (
     <div>
-      <h1>{token}</h1>
+      <h1>Falso</h1>
     </div>
   )
 }
