70bd0bbd32056cbaa7d76766ac747bd611ad00f1
Tales Félix <tales.felix.10@gmail.com>
2022-10-24 15:50:05 -0300

    FEAT(components/layout): Adicionar layout nas paginas

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,28 +7,31 @@ import { IsAuth } from '../components/is-auth/index.js'
 import { ChallengePage } from '../pages/challenges'
 import Evaluation from '../pages/evaluation'
 import MentorPage from '../pages/user'
+import { Layout } from '../components/layout'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} exact />
-        <Route path="/home" element={<HomePage />} exact />
-        <Route path="/hiring-process" element={<IsAuth />}>
-          <Route path="/hiring-process" element={<HiringProcessPage />} exact />
-        </Route>
-        <Route path="/user" element={<IsAuth mentorType={['admin']} />}>
-          <Route path="/user" element={<MentorPage />} exact />
-          <Route path="/user/change-password/:id" element={<UserChangePasswordPage />} exact />
-        </Route>
-        <Route path="/challenges" element={<IsAuth />}>
-          <Route path="/challenges/hiring-process/:id" element={<ChallengePage />} exact />
-        </Route>
-        <Route path="/exercise" element={<IsAuth />}>
-          <Route path="/exercise/:id" element={<Evaluation />} exact />
+        <Route path="" element={<Layout />}>
+          <Route path="/home" element={<HomePage />} exact />
+          <Route path="/hiring-process" element={<IsAuth />}>
+            <Route path="/hiring-process" element={<HiringProcessPage />} exact />
+          </Route>
+          <Route path="/user" element={<IsAuth mentorType={['admin']} />}>
+            <Route path="/user" element={<MentorPage />} exact />
+            <Route path="/user/change-password/:id" element={<UserChangePasswordPage />} exact />
+          </Route>
+          <Route path="/challenges" element={<IsAuth />}>
+            <Route path="/challenges/hiring-process/:id" element={<ChallengePage />} exact />
+          </Route>
+          <Route path="/exercise" element={<IsAuth />}>
+            <Route path="/exercise/:id" element={<Evaluation />} exact />
+          </Route>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter >
   )
 }
 
