15544870433c49eae8c8ce649f48b4ed9d16abad
Emilly <emillymarques96622@gmail.com>
2022-10-24 15:50:46 -0300

    feat:passando funcionalidade de refresh e anulando valor do token - @vanessalmirante @MarquesEmilly2

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,26 +2,24 @@ import DefaultButton from '../buttons/default'
 import { faDoorOpen } from '@fortawesome/free-solid-svg-icons'
 import { HeaderComponent } from './styled'
 // import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router'
+// import { useNavigate } from 'react-router'
 
 const Layout = () => {
   // const { t } = useTranslation()
-
+  // const navigate = useNavigate()
   const handleClick = (e) => {
     e.preventDefault()
     const handleToken = localStorage.token
-    
-    if (handleToken){
-    localStorage.setItem('token', null)
-    useNavigate('/')
+    if (handleToken) {
+      localStorage.setItem('token', null)
+      window.location.reload()
     }
-    
   }
 
   return (
     <>
       <HeaderComponent>
-        <DefaultButton icon={faDoorOpen} onClick={handleClick} />
+        <DefaultButton icon={faDoorOpen} onClick={handleClick()} />
       </HeaderComponent>
     </>
 
