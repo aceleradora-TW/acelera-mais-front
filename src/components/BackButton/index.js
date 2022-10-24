9240581720468c06713d3b7c78f196e169f00a75
Tales Félix <tales.felix.10@gmail.com>
2022-10-24 10:49:26 -0300

    refactor(components/BackButton): refatorar nome do componente e props

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -2,10 +2,10 @@ import Button from '../buttons/button'
 import { faReply } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
 
-const BackButton = ({ icon, className }) => {
+const BackButton = ({ className }) => {
   const navigate = useNavigate()
   return (
-    <Button icon={icon || faReply} onClick={() => navigate(-1)} className={className} />
+    <Button icon={faReply} onClick={() => navigate(-1)} className={className} />
   )
 }
 
