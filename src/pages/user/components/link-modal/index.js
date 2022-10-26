c28467fc71510277af5aa6a54ca1901112f11521
Erick Cardoso <cyber666king@gmail.com>
2022-10-26 11:05:39 -0300

    Repara erros indicados pelo code review

--- src/pages/user/components/link-modal/index.js
+++ src/pages/user/components/link-modal/index.js
@@ -1,15 +1,20 @@
 import { Modal } from '../../../../components/modal'
+import { useTranslation } from 'react-i18next'
 import { UserLink } from '../user-link'
 
-export const LinkModal = ({ method = 'GET', icon }) => {
+export const CreateLink = ({ method = 'GET', icon }) => {
+  const { t } = useTranslation()
   return (
     <div>
       <Modal
         icon={icon}
+        title={t('Gerar Link')}
         className='button primary'
+        text={t('Gerar Convite')}
       >
         <UserLink method={method} />
       </Modal>
+
     </div>
   )
 }
