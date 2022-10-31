06235f7a15c1091fba605a37037f50cfeaf763a4
Tales Félix <tales.felix.10@gmail.com>
2022-10-31 13:43:37 -0300

    corringindo regra de visibilidade do botão nas paginas

--- src/components/buttons/backButton/index.js
+++ src/components/buttons/backButton/index.js
@@ -1,10 +1,18 @@
 import Button from '../button/index'
 import { faReply } from '@fortawesome/free-solid-svg-icons'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 export const BackButton = ({ className }) => {
   const navigate = useNavigate()
+
+  const isVisibleForPath = () => {
+    const { pathname } = useLocation()
+    const notVisiblePaths = ['home', 'user/change-password']
+    return notVisiblePaths.some(notVisiblePath => pathname.includes(notVisiblePath))
+  }
+
   return (
+    !isVisibleForPath() &&
     <Button icon={faReply} onClick={() => navigate(-1)} className={className} />
   )
 }
