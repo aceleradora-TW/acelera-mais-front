ea8f4661e5e9cc25b1507b0299004026f8acfb7f
renatu5 <renatoosoouzaa26@gmail.com>
2022-11-04 10:47:02 -0300

    feat(buttons/sort/styled): ajustando estilizacao do component StyledSortButton
    @Renatu5 - @EnzoSoares-dev
    
    170

--- src/components/buttons/sort/styled.js
+++ src/components/buttons/sort/styled.js
@@ -2,10 +2,15 @@ import styled from 'styled-components'
 
 export const StyledSortButton = styled.button`
   border: none;
-  display: flex;  
+  display: flex;
+  margin-left: 35px;
   padding: 10px 25px;
   background-color: #fbfbfb;
   font-weight: bold;
   font-size: 14px;
   text-transform: uppercase;
+  &:hover {
+   cursor: pointer;
+   opacity: 0.7
+  } 
 `
