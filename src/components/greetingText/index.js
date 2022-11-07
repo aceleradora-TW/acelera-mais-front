7c36bd6bb01c342c540eb9f58f485fd3b651a853
Erick Cardoso <cyber666king@gmail.com>
2022-11-07 10:00:59 -0300

    feat(greetingTExt/index.js): implementa i18next no texto de pessoa logada - @cibely87 @erickcardoso007

--- src/components/greetingText/index.js
+++ src/components/greetingText/index.js
@@ -1,11 +1,8 @@
 import { Greeting } from './styled'
 import { useTranslation } from 'react-i18next'
 
-const { t } = useTranslation()
-
 export const GreetingText = () => {
+  const { t } = useTranslation()
   const nome = localStorage.getItem('mentorName').split(' ').shift()
-  return <Greeting>Seja bem vinde, <span>{nome}</span></Greeting>
+  return <Greeting>{t('layout.greetingText.Greeting', { nome: nome })}</Greeting>
 }
-
-// t('layout.greetingText.Greeting', { nome: nome })
