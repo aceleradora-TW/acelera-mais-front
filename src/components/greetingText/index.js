3b6e02dc6d61b7ebc95b68efb867db6c1190dd89
Cibely Pereira <cibely.port@gmail.com>
2022-11-09 14:55:44 -0300

    Fix:resolucao de conflito, refatoracao nome de variavel @erickcardoso007 @janinelps @cibely87

--- src/components/greetingText/index.js
+++ src/components/greetingText/index.js
@@ -3,6 +3,6 @@ import { useTranslation } from 'react-i18next'
 
 export const GreetingText = () => {
   const { t } = useTranslation()
-  const nome = localStorage.getItem('mentorName').split(' ').shift()
-  return <Greeting>{t('layout.greetingText.Greeting', { nome: nome })}</Greeting>
+  const name = localStorage.getItem('mentorName').split(' ').shift()
+  return <Greeting>{t('layout.greetingText.Greeting', { name: name })}</Greeting>
 }
--- src/components/greetingText/styled.js
+++ src/components/greetingText/styled.js
@@ -4,7 +4,4 @@ export const Greeting = styled.p`
   margin: 0;
   padding: 10px 0px;
   color: black;
-  span {
-    text-transform: capitalize;
-  }
 `
