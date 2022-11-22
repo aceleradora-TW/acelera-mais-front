68a0f7f687959ce714005b1ac600932ff2bc1b81
nicofercavv-dev <nicoleferreira281@gmail.com>
2022-11-22 15:19:17 -0300

    refactor: salvar dados da criação de hiring process inacabada para quando abrir de novo serem mostradas nos imputs @EnzoSoares-dev @nicofercavv-dev

--- src/components/inputs/text/index.js
+++ src/components/inputs/text/index.js
@@ -1,7 +1,7 @@
 import { Input } from '../index'
 
-export const InputText = ({ label, onChange, name, placeholder }) => {
+export const InputText = ({ label, onChange, onBlur, name, placeholder, value }) => {
   return (
-    <Input type="text" name={name} label={label} placeholder={placeholder} onChange={onChange} />
+    <Input type="text" name={name} value={value} label={label} placeholder={placeholder} onChange={onChange} onBlur={onBlur}/>
   )
 }
