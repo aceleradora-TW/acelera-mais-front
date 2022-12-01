448969391dc26441d27bbd214029fe07a6c8fafe
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-12-01 15:18:52 -0300

    feat(src/pages/evaluation/components/select-note/select-note.js): Validando preenchimento do campo nota e feedback -@oGuilhermeSoares -@cintia89

--- src/pages/evaluation/components/select-note/select-note.js
+++ src/pages/evaluation/components/select-note/select-note.js
@@ -30,26 +30,39 @@ export const Score = ({ exercise }) => {
     history.back()
   }
 
+  const isANumber = () => Number.isNaN(Number(score))
+
   return (
     <ScoreContainer>
-
-      < Select
+      <Select
         label={t('evaluation.score.title')}
-        placeholder={t('evaluation.score.placeholder')}
+      placeholder={t('evaluation.score.placeholder')}
         onChange={handleScore}
-        options={
-          [
-            { label: 0, value: 0 },
-            { label: 1, value: 1 },
-            { label: 2, value: 2 },
-            { label: 3, value: 3 },
-            { label: 4, value: 4 },
-            { label: 5, value: 5 }
-          ]} ></Select >
+       // required={true}
+        options={[
+          { label: 0, value: 0 },
+          { label: 1, value: 1 },
+          { label: 2, value: 2 },
+          { label: 3, value: 3 },
+          { label: 4, value: 4 },
+          { label: 5, value: 5 }
+        ]}
+      ></Select>
 
-      <textarea placeholder={t('evaluation.score.placeholderTextarea')} className="form-control" id="message-text" onChange={handleTextArea} ></textarea>
-      <PrimaryButton text={t('evaluation.score.sendButton')} onClick={handleSubmit} />
+      <textarea
+        placeholder={t('evaluation.score.placeholderTextarea')}
+        className="form-control"
+        id="message-text"
+        onChange={handleTextArea}
+      >
 
+      </textarea>
+
+      <PrimaryButton
+        text={t('evaluation.score.sendButton')}
+        onClick={handleSubmit}
+        disabled={isANumber() || !feedback}
+      />
     </ScoreContainer>
   )
 }
