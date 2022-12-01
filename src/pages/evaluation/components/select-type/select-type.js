f40b64908cfc4b696edd204481d62294a8c3c5d7
Tales Félix <tales.felix.10@gmail.com>
2022-12-01 15:38:29 -0300

    REFACTOR(src/evaluation): Mostrando tipo de exercicio na tela @EnzoSoares-dev @talesacademico

--- src/pages/evaluation/components/select-type/select-type.js
+++ src/pages/evaluation/components/select-type/select-type.js
@@ -5,9 +5,9 @@ import SucessButton from '../../../../components/buttons/sucess'
 import { TypeContainer } from '../../styled'
 import { useTranslation } from 'react-i18next'
 
-export const Type = ({ setDisableEvaluationButton }) => {
+export const Type = ({ setDisableEvaluationButton, defaultType }) => {
   const [challengeTypeSelected, setChallengeTypeSelected] = useState(false)
-  const [exerciseType, setExerciseType] = useState()
+  const [exerciseType, setExerciseType] = useState(defaultType)
   const { t } = useTranslation()
 
   const id = window.location.pathname.split('/')[2]
@@ -26,7 +26,7 @@ export const Type = ({ setDisableEvaluationButton }) => {
           setChallengeTypeSelected(true)
         }}
         label={t('exercise.type.title')}
-        placeholder={t('exercise.type.placeholder')}
+        placeholder={defaultType || t('exercise.type.placeholder')}
         options={[
           { label: 'Backend', value: 'Backend' },
           { label: 'Frontend', value: 'Frontend' },
