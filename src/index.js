d32f36ae2cc59f4cff009291dee5e2fee72beb89
nicofercavv-dev <nicoleferreira281@gmail.com>
2022-12-15 14:13:06 -0300

    refactor(src/index.js): deixando o código mais limpo @cintia89 @nicofercavv-dev

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client'
 import './utils/i18next'
 import App from './App'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-
+const root = createRoot(document.getElementById('root'))
 root.render(<App tab='home' />)
